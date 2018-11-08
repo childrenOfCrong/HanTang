5e2f294f480309715dcf922d8fef5913b91135ec
chany <lyhy0310@gmail.com>
2018-11-08 19:35:04 +0900

    [add]  돈 정보 style fix

--- src/js/View/template.js
+++ src/js/View/template.js
@@ -4,7 +4,9 @@ export const playerTemplate = ({ Id, cardSet, profile, gameResult, money, score,
     <div class="avatarBox">
       <div class="avatar" alt="동그란 유저 사진"></div>
     </div>
-    <div class="money">${money}</div>
+    <div class="moneyBox">
+      <span class="moneyPicture"></span><span class="money">${money} 냥</span>
+    </div>
   </div>
   <div class="cardContainer">
     <div class="cardSet">
